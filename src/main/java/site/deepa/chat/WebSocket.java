243cad70a5c8d6fa1a631eab765b058505827764
Deepalakshmi Thiagarajan <deepa.name@gmail.com>
2018-09-17 22:07:58 -0700

    add websocket server that echoes input

---
+++ src/main/java/site/deepa/chat/WebSocket.java
@@ -0,0 +1,33 @@
+package site.deepa.chat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+@Configuration
+public class WebSocket {
+
+    @Bean
+    public WebSocketHandlerAdapter handlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+
+    @Bean
+    public HandlerMapping webSockeMapping() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("/ws", new EchoSocketHandler());
+
+        SimpleUrlHandlerMapping simpleUrlHandlerMapping = new SimpleUrlHandlerMapping();
+        simpleUrlHandlerMapping.setUrlMap(map);
+
+        simpleUrlHandlerMapping.setOrder(10);
+        return simpleUrlHandlerMapping;
+
+    }
+
+}
